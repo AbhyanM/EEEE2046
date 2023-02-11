8808deb2be6836030e3fd64e147ffb3370c7932c
plevans <31508477+plevans@users.noreply.github.com>
2023-02-11 17:59:53 +0000

    Update VRRenderThread.h
    Update out of date comment in VRRenderThread.h

--- WorksheetCode/Worksheet6/VRRenderThread/VRRenderThread.h
+++ WorksheetCode/Worksheet6/VRRenderThread/VRRenderThread.h
@@ -28,10 +28,7 @@
 
 
 /* Note that this class inherits from the Qt class QThread which allows it to be a parallel thread
- * to the main() thread, and also from vtkCommand which allows it to act as a "callback" for the 
- * vtkRenderWindowInteractor. This callback functionallity means that once the renderWindowInteractor
- * takes control of this thread to enable VR, it can callback to a function in the class to check to see
- * if the user has requested any changes
+ * to the main() thread.
  */
 class VRRenderThread : public QThread {
     Q_OBJECT
